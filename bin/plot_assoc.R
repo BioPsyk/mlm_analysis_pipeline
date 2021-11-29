ec226f357c1b12dfd102a73d83c1f9b1249a5a09
vaqm2 <appadurai.vivek@gmail.com>
2021-11-29 09:34:32 +0100

    col names changed in saige out

--- bin/plot_assoc.R
+++ bin/plot_assoc.R
@@ -16,7 +16,8 @@ if("Is.SPA.converge" %in% colnames(assoc)) {
 if("Is.converge" %in% colnames(assoc)) {
     assoc = assoc %>% filter(Is.converge == 1)
 }
-assoc = assoc %>% select(CHR, SNP, BP, P)
+assoc = assoc %>% select(CHR, SNPID, POS, `p.value`)
+colnanes(assoc) = c("CHR", "SNP", "BP", "P")
 
 png(paste(out_prefix, "Manhattan.png", sep = "_"),
     width = 8, 
